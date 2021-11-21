346d81e063c5b5518d16bad85e3deb2afcd5a4f5
Adwaita Raj Modak <adwaitaraj@gmail.com>
2021-11-22 02:28:32 +0530

    buttonEdited

--- ritual-web/src/components/Home.jsx
+++ ritual-web/src/components/Home.jsx
@@ -5,6 +5,7 @@ function Home() {
     return (
         <div className='home'>
             <img src={logo} className="App-logo" alt="logo" />
+            <button className="bbtn">Shop All</button>
 
         </div>
     )
